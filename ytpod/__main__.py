63083b9ed37bff91583d024ce6c7df0b9c19717f
Adam Montano <amontano495@gmail.com>
2024-11-06 11:05:28 -0800

    downloads in mp3 and also uses a url file now

--- ytpod/__main__.py
+++ ytpod/__main__.py
@@ -1,29 +1,35 @@
 import feedparser
 import yt_dlp
 import json
+import sys
 from datetime import date,datetime
+def main():
+    url_file = open("urls","r")
+    for rss_url in url_file:
+    
+        feed = feedparser.parse(rss_url)
 
-rss_url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCH8JwgaHCkhdfERVkGbLl2g"
-feed = feedparser.parse(rss_url)
+        current_date = date.today()
 
-current_date = date.today()
+        for entry in feed.entries:
+            entry_date = datetime.strptime(entry.published[:10],'%Y-%m-%d').date()
+            delta = int((current_date-entry_date).days)
+            if delta <= 1:
+                URLS = [entry.link]
+                ydl_opts = {
+                    'format': 'm4a/bestaudio/best',
+                    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+                    'postprocessors': [{  # Extract audio using ffmpeg
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                    }]
+                }
 
-for entry in feed.entries:
-    entry_date = datetime.strptime(entry.published[:10],'%Y-%m-%d').date()
-    delta = int((current_date-entry_date).days)
-    if delta <= 1:
-        URLS = [entry.link]
-        ydl_opts = {
-            'format': 'm4a/bestaudio/best',
-            # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-            'postprocessors': [{  # Extract audio using ffmpeg
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'm4a',
-            }]
-        }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    error_code = ydl.download(URLS)
+    sys.exit(0)
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            error_code = ydl.download(URLS)
+main()
 
 '''
     print("From",delta,"days ago")
